9effa3d5b1c70b7a45a0dcf8ec6fc9a15c2bd808
Kamaal Farah <kamaal.f1@gmail.com>
2023-07-26 18:36:18 +0200

    Starting the clock

---
+++ UnixTime/ClockManager.swift
@@ -0,0 +1,29 @@
+//
+//  ClockManager.swift
+//  UnixTime
+//
+//  Created by Kamaal M Farah on 26/07/2023.
+//
+
+import Foundation
+import KamaalExtensions
+
+final class ClockManager: ObservableObject {
+    @Published private var time: TimeInterval
+
+    init() {
+        self.time = Date().timeIntervalSince1970
+
+        startCounting()
+    }
+
+    var formattedTime: String {
+        String(time.int)
+    }
+
+    private func startCounting() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+            self?.time = Date().timeIntervalSince1970
+        }
+    }
+}
--- UnixTime/ContentView.swift
+++ UnixTime/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var clockManager = ClockManager()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(clockManager.formattedTime)
         }
         .padding()
     }
