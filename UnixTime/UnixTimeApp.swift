c37c16ad7a5bc98c85482da8e4ae85e8576da754
Kamaal Farah <kamaal.f1@gmail.com>
2023-07-26 18:11:40 +0200

    Initial commit

---
+++ UnixTime/UnixTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnixTimeApp.swift
+//  UnixTime
+//
+//  Created by Kamaal Farah on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct UnixTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
